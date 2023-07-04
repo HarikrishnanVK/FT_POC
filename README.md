46889af0f50f8233c2813b566b6267f1fd74ee26
HarikrishnanVK <harikrishnan301293@gmail.com>
2023-07-04 21:31:22 +0530

    my first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# First step is to run the Docker compose file to setup grid with hubs and nodes#
+
+Command to run : docker compose up (this has to be run from repository root path with powershell terminal)
+
+# Second step is to build the node modules #
+
+npm install
+
+# Third step is to trigger the conf.ts file #
+
+Use script inside package.json => npm run test
---
+++ conf.ts
@@ -0,0 +1,79 @@
+import { Config, browser } from 'protractor';
+var AllureReporter = require('jasmine-allure-reporter');
+
+let SpecReporter = require('jasmine-spec-reporter').SpecReporter;
+
+//var screenreporter = require('util/screenreporter.js');
+
+export let config: Config = {
+  directconnect: false,
+    framework: 'jasmine',
+    jasmineNodeOpts: {
+        showColors: true,
+        defaultTimeoutInterval: 30000,
+        silent: true
+    },
+    allScriptsTimeout: 30000,
+    // seleniumAddress: 'http://localhost:4455/wd/hub',
+    multiCapabilities: [
+        {
+            seleniumAddress: 'http://localhost:4445/wd/hub',
+            browserName: 'chrome',
+            specs: ['specs/suite1/*.spec.js'],
+            shardTestFiles: true,
+            maxInstances: 4,
+        },
+        {
+            seleniumAddress: 'http://localhost:4445/wd/hub',
+            browserName: 'firefox',
+            specs: ['specs/suite1/*.spec.js'],
+            shardTestFiles: true,
+            maxInstances: 4,
+        },
+        {
+            seleniumAddress: 'http://localhost:4455/wd/hub',
+            browserName: 'chrome',
+            specs: ['specs/suite2/*.spec.js'],
+            shardTestFiles: true,
+            maxInstances: 3,
+        },
+        {
+            seleniumAddress: 'http://localhost:4455/wd/hub',
+            browserName: 'firefox',
+            specs: ['specs/suite2/*.spec.js'],
+            shardTestFiles: true,
+            maxInstances: 3,
+        }
+    ],
+
+  // suites: {
+  //   calc: './specs/calculator.js',
+  //   //to run each suite
+  //   //protractor conf.js calc
+  //   //protractor conf.js bank
+  //   //protractor conf.js calc,bank
+  //   //protractor conf.js bothtest
+  // },
+
+
+  onPrepare: () => {
+    let globals = require('protractor');
+    let browser = globals.browser;
+    browser.ignoreSynchronization = true;
+    // browser.manage().window().maximize();
+    browser.manage().timeouts().implicitlyWait(10000);
+    // doing a browser.get will lead to a transpile error. 
+    // undefined does not have a get method
+
+      /* Jasmine spec reporter */
+      var SpecReporter = require('jasmine-spec-reporter').SpecReporter;
+      jasmine.getEnv().addReporter(new SpecReporter({ displayStacktrace: 'all' }));
+
+      jasmine.getEnv().addReporter(new AllureReporter({
+          resultsDir: 'allure-results'
+      }));
+
+     
+      }
+    }
+
